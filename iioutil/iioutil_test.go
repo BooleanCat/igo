c7221c2a2050ee830ec1521f7d50113bf4f2f31d
BooleanCat <thgodkin@gmail.com>
2017-01-10 19:20:02 +0000

    remove stuttering

--- iioutil/iioutil_test.go
+++ iioutil/iioutil_test.go
@@ -7,15 +7,11 @@ import (
 )
 
 var _ = Describe("iioutil", func() {
-	Describe("Ioutil", func() {
-		It("is implemented by IoutilWrap", func() {
-			ioutil := new(iioutil.IoutilWrap)
-			var _ = iioutil.Ioutil(ioutil)
-		})
+	It("is implemented by Real", func() {
+		var _ iioutil.Ioutil = iioutil.New()
+	})
 
-		It("is implemented by IoutilFake", func() {
-			ioutil := new(iioutil.IoutilFake)
-			var _ = iioutil.Ioutil(ioutil)
-		})
+	It("is implemented by Fake", func() {
+		var _ iioutil.Ioutil = iioutil.NewFake()
 	})
 })
